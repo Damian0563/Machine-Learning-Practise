72b103bddff41cf0625d34f1e00e4cfbf35d0f08
Damian0563 <damian.realmadrid2005@gmail.com>
2025-05-23 09:30:43 +0200

    Workflow setup for OneHotEncoder in SocialMedia_Productivity

--- SocialMedia_Productivity/main.py
+++ SocialMedia_Productivity/main.py
@@ -1,6 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
+from sklearn.metrics import classification_report
 
 file=pd.read_csv('social_media_vs_productivity.csv')
 data=pd.DataFrame(file)
+
+print(data.info())
+data.dropna(inplace=True)
+data.drop_duplicates(inplace=True)
+
+oh=OneHotEncoder()
+gender_enc=oh.fit_transform(data[['gender']]).toarray()
+temp_gender=pd.DataFrame(gender_enc,columns=oh.get_feature_names_out(['gender']))
+data=pd.concat([data.drop('gender', axis=1), temp_gender], axis=1)
+
+
+
