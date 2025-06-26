d86b4e529c41f2a42a18e15916021fa18272043e
Damian0563 <damian.realmadrid2005@gmail.com>
2025-06-26 08:37:43 +0200

    visual tumor classification

--- Visual_Tumor_Classification/main.py
+++ Visual_Tumor_Classification/main.py
@@ -4,9 +4,11 @@ from skimage.io import imread #type:ignore
 from skimage.transform import resize #type:ignore
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix, make_scorer, f1_score
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 import seaborn as sns #type:ignore
+import joblib
 import os
 
 def label_encode(df_train,df_test):
@@ -15,10 +17,10 @@ def label_encode(df_train,df_test):
     df_test=encoder.transform(df_test)
     return df_train, df_test
 
-BASE_DIR='/Users/damia/Desktop/Machine Learning/LinearPerformance/Visual_Tumor_Classification'
+BASE_DIR='/Users/damia/Desktop/Machine Learning/Visual_Tumor_Classification'
 categories=['Training','Testing']
 labels=['glioma','meningioma','notumor','pituitary']
-
+print('Preprocessing images and splitting...')
 x_train, x_test, y_train, y_test=[],[],[],[]
 for category in categories:
     for label in labels:
@@ -26,16 +28,53 @@ for category in categories:
         for file in os.listdir(path):
             img_path=os.path.join(path,file)
             image=imread(img_path)
-            image=resize(image,(16,16))
-            if category=="Training":
-                x_train.append(image.flatten())
-                y_train.append(label)
-            else:
-                x_test.append(image.flatten())
-                y_test.append(label)
-
-x_train = np.array(x_train)
-x_test = np.array(x_test)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
+            if image.ndim == 2:
+                image = np.stack([image] * 3, axis=-1)  # grayscale to RGB
+            elif image.shape[2] == 4:
+                image = image[:, :, :3]  # RGBA to RGB
+            image=resize(image,(16,16),anti_aliasing=True)
+            image=image.astype(np.float32)
+            if image.shape == (16,16,3):
+                if category=="Training":
+                    x_train.append(image.flatten())
+                    y_train.append(label)
+                else:
+                    x_test.append(image.flatten())
+                    y_test.append(label)
+print('Split done...')
+x_train = np.asarray(x_train)
+x_test = np.asarray(x_test)
+y_train = np.asarray(y_train)
+y_test = np.asarray(y_test)
 y_train,y_test=label_encode(y_train,y_test)
+print('Done encoding...')
+weighted=make_scorer(f1_score,average='weighted')
+est=SVC()
+params={
+    'C':[0.1,1,10],
+    'kernel':['rbf','linear'],
+    'gamma':['auto','scale']
+}
+grid=GridSearchCV(
+    estimator=est,
+    param_grid=params,
+    scoring=weighted,
+    cv=5,
+    n_jobs=-1,
+    verbose=2
+)
+print('Optimising for the most efficient model parameters...')
+model=grid.fit(x_train,y_train)
+print(grid.best_params_) #==>> {'C': 10, 'gamma': 'scale', 'kernel': 'rbf'}
+joblib.dump(model,'optimized')
+model=joblib.load('optimized')
+
+y_pred=model.predict(x_test)
+print(classification_report(y_test,y_pred))
+
+plt.figure(figsize=(10,6))
+plt.title('Confussion matrix')
+ax=sns.heatmap(confusion_matrix(y_test,y_pred),annot=True,cbar=True, fmt='d',cmap='Blues')
+ax.set_xticklabels(['glioma','meningioma','No Tumor','Pituitary'])
+ax.set_yticklabels(['glioma','meningioma','No Tumor','Pituitary'])
+plt.show()
---
+++ Visual_Tumor_Classification/readme.md
@@ -0,0 +1,12 @@
+## Introduction
+The following repo contains a well-known Machine Learning problem within the paradigm of Computer Vision. The brain MRI dataset (https://www.kaggle.com/datasets/masoudnickparvar/brain-tumor-mri-dataset) is used. SVM model provides a high accuracy and efficiency.
+
+## Case Study
+<img src="plots/confussion.png" alt="confussion matrix" width="600">
+<img src="plots/stats.png" alt="confussion matrix" width="600">
+<strong>Where:<br>
+        0: Glioma<br>
+        1: Meningioma<br>
+        2: No Tumor<br>
+        3: Pituitary
+</strong>
