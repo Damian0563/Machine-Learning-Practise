06e1d4387c24b6c722fa4236619983ff5784281f
Damian0563 <damian.realmadrid2005@gmail.com>
2025-07-21 06:10:37 +0200

    set up for preprocessing

--- Kidney_Stones_Classification/main.py
+++ Kidney_Stones_Classification/main.py
@@ -9,4 +9,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns #type:ignore
 import os
 
-
+classifications=['normal','stone']
+normal,stones=[],[]
+for option in classifications:
+    path=f"{os.getcwd()}/{option}"
+    for file in os.listdir(path):
+        file=os.path.join(path,file)
+        image=imread(file)
+        image=resize(image,(24,24),anti_aliasing=True)
+        print(image.ndim)
+        os._exit(1)
+        if option=='normal': normal.append(image)
+        else: stones.append(image)
