c1e309a64159f905d0a84c8e5688cd7a8789d1d9
Damian0563 <damian.realmadrid2005@gmail.com>
2025-07-24 06:14:21 +0200

    tensorflow model trainging

--- Kidney_Stones_Classification/main.py
+++ Kidney_Stones_Classification/main.py
@@ -42,18 +42,38 @@ for option in classifications:
                     y_test.append(1)
                 stone_itr+=1
 print("Concluded preprocessing data images and training-testing splits.")
-model=tf.keras.models.Sequential([
-    tf.keras.layers.Dense(128,activation="sigmoid",input_shape=(x_train.shape[1],)),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(1,activation="sigmoid")
-]
-)
-loss_function=tf.keras.losses.BinaryCrossentropy()
-model.compile(optimizer="adam",loss=loss_function,metrics=['accuracy'])
-print("Training model...")
-model.fit(x_train,y_train, epochs=5)
-evaluation=model.evaluate(x_test,y_test,verbose=2)
-loss,accuracy=evaluation[0],evaluation[1]
-print(f"Model loss: {loss}, model accuracy: {accuracy}")
-joblib.dump(model,"trained")
-model=joblib.load("trained")
+try:
+    model=tf.keras.models.Sequential([
+        tf.keras.layers.Dense(128,activation="sigmoid"),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1,activation="sigmoid")]
+    )
+    loss_function=tf.keras.losses.BinaryCrossentropy()
+    model.compile(optimizer="adam",loss=loss_function,metrics=['accuracy'])
+    print("Training model...")
+    model.fit(x_train,y_train,batch_size=200,epochs=5,verbose=0)
+    evaluation=model.evaluate(x_test,y_test,verbose=2)
+    print(f"Model evaluation: {evaluation}")
+    joblib.dump(model,"trained")
+except Exception as e:
+    print(e)
+
+#plt.figure(figsize=(10,5))
+#plt.plot(history.history['accuracy'])
+#plt.title('Model accuracy')
+#plt.ylabel('Accuracy')
+#plt.xlabel('Epochs')
+#plt.show()
+
+#plt.figure(figsize=(10,5))
+#plt.plot(history.history['loss'])
+#plt.title('Model loss')
+#plt.ylabel('Loss')
+#plt.xlabel('Epochs')
+#plt.show()
+
+
+
+
+
+
